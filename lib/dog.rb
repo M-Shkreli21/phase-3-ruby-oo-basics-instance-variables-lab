fa7e9890e6b260c6609bc32e9f28bb1e149793f7
Mark Shkreli <m.shkreli96@icloud.com>
2022-11-22 14:17:23 -0500

    commit tree 1a9631ae316c4096aef154e1ab8eaf5e94c3f6e9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    def name=(dog_name) #Setter method
+        @this_dogs_name = dog_name
+    end
 
+    def name #Getter method
+        @this_dogs_name
+    end
+
+    lassie = Dog.new
+    lassie.name = "Lassie"
+
+    puts lassie.name
 end
